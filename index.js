5e6fe3d4babfc577f6c2507bdf6bc2f7ba07af93
PamirKalo <kbm2@mail.bg>
2023-05-24 09:58:17 +0300

    Update index.js refactor paint function

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function paint(color) {
     const circle = document.getElementById('circleID');
-    circle.style = `background-color: ${color}`;
+    let rgb_color = null;
+    if (typeof color === 'undefined') {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        rgb_color = `rgb(${r}, ${g}, ${b})`;
+    }
+    circle.style = `background-color: ${color || rgb_color}`;
 }
